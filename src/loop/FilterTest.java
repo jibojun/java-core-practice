3cccabecbf352db7a68cf5514e03ab7a98e53b25
jibojun <jbj19910123@163.com>
2018-08-05 17:57:14 +0800

    test

--- src/loop/FilterTest.java
+++ src/loop/FilterTest.java
@@ -3,6 +3,7 @@ package loop;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @Author: Bojun Ji
@@ -11,17 +12,21 @@ import java.util.Optional;
  */
 public class FilterTest {
     public static void main(String[] args) {
-        List<String> list=prepareList();
-        Optional optional=list.stream().filter(s -> s.isEmpty()).findFirst();
-        if(optional.isPresent()){
-            System.out.println("found");
-        }else{
-            System.out.println("not found");
+        List<String> list = prepareList();
+        Optional optional1 = list.stream().filter(s -> s.isEmpty()).findFirst();
+        if (optional1.isPresent()) {
+            System.out.println("found empty string");
+        } else {
+            System.out.println("not found empty string");
         }
+        List<String> resultList = list.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        System.out.println(resultList);
+        list.stream().filter(s -> !s.isEmpty()).forEach(s -> System.out.println(s));
+
     }
 
-    private static List<String> prepareList(){
-        List<String> list=new ArrayList<>();
+    private static List<String> prepareList() {
+        List<String> list = new ArrayList<>();
         list.add("test1");
         list.add("test2");
         list.add("");
